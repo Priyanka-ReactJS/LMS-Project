84406cfb6f17c4b0ca5d5864fa1498880b0eafa0
priyanka211992 <pppatel211992@gmail.com>
2020-08-12 11:26:14 -0400

    LMS-Project

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore ,combineReducers} from 'redux';
+import {createStore ,combineReducers , compose} from 'redux';
 import {Provider} from 'react-redux';
 
 import './index.css';
@@ -14,12 +14,16 @@ const rootReducers = combineReducers({
    studentState:student
 })
 
-const store = createStore(rootReducers);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(rootReducers , composeEnhancers());
 
 ReactDOM.render(
+  <Provider store= {store}>
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
 
--- src/store/reducers/course.js
+++ src/store/reducers/course.js
@@ -1,4 +1,17 @@
-const initialState = {course: []};
+const initialState = {course: [{
+    id:1,
+    courseName : 'First Course',
+    category:1,
+    subCategory:2,
+    fees:1000,
+},
+{
+    id:2,
+    courseName : 'Second Course',
+    category:2,
+    subCategory:1,
+    fees:2000,
+}]};
 
  function course(state=initialState, action) {
      console.log("course...",state)
